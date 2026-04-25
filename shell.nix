b78da6f7927a1b4964396995eb1095cc498edf98
Ben Landrette <128928536+BendyLand@users.noreply.github.com>
2026-04-25 19:03:47 -0400

    Update env setup

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-25.11.tar.gz";
+  pkgs = import nixpkgs {};
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.zig_0_14
+    pkgs.zls
+    pkgs.pkg-config
+    pkgs.xorg.libX11
+    pkgs.xorg.libXfixes
+  ];
+
+  shellHook = ''
+    echo "--- zclip development environment (Zig 0.14.1) ---"
+  '';
+}
